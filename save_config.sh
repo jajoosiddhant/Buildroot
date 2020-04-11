9222d60d770483c5d20e522fe7ee8ff76f162f03
Siddhant Jajoo <sija6678@colorado.edu>
2020-04-11 01:35:28 -0600

    Adding save_config.sh script

---
+++ save_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Script to save the modified configuration as modified_qemu_aarch64_virt_defconfig and linux kernel configuration.
+#Author: Siddhant Jajoo.
+
+cd `dirname $0`
+source shared.sh
+mkdir -p base_external/configs/
+make -C buildroot savedefconfig BR2_DEFCONFIG=${MODIFIED_DEFCONFIG_REL_BUILDROOT}
+
+if [ -e buildroot/.config ] && [ -e buildroot/output/build/linux-*/.config ]; then
+	grep "BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE" buildroot/.config > /dev/null
+	rc=$?
+	if [ $rc -eq 0 ]; then
+		echo "Saving linux defconfig"
+		make -C buildroot linux-update-defconfig
+	fi
+fi
